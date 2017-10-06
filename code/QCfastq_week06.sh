dbab2bc3f1e10b96e0582cb15583a00aa6af1825
Alfredo Hans Locht <halocht@gmail.com ahlocht@dons.usfca.edu>
2017-10-06 18:24:48 +0000

    .sh at the end of a script matters!

--- code/QCfastq_week06.sh
+++ code/QCfastq_week06.sh
@@ -6,7 +6,7 @@
 
 # This is a QC script to extract the SRA numbers from some fastq files and then process them further
 
-# Script's name: QCfastq_week06
+# Script's name: QCfastq_week06.sh
 
 echo "Checking the value of BLASTDB variable"
 
@@ -23,10 +23,17 @@ echo "Downloading fastq files into data/raw_data"
 # the pipe and tail -n +2 is a handy way to exclude the first line
 for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
 do
-    fastq-dump -v $SRA_number -O halocht-week06-homework-QC-and-BLAST/data/raw_data
+    fastq-dump -v $SRA_number -O data/raw_data
 done
 
-echo "Done downloading fastq files into ../data/raw_data"
+echo "Done downloading fastq files into /data/raw_data"
 
-cat halocht-week06-homework-QC-and-BLAST/data/raw_data
+echo "#####################################################"
+
+echo "Fastqc"
+# Now run Fastqc to discard sequences less than 150 bp, set leading or trailing parameters, and cut off reads if they obtain score of less than 25
+
+echo "Running fastqc"
+
+fastqc data/raw_data/*.fastq --outdir=output/fastqc
 
